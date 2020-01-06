bb84a9391e00be0aecd8bd4940d24130417c1463
Rafał Borowski <rafall.borowski@gmail.com>
2020-01-06 23:45:46 +0100

    card component

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 import Paragraph from 'components/atoms/Paragraph/Paragraph';
 import Heading from 'components/atoms/Heading/Heading';
 import Button from 'components/atoms/Button/Button';
+import LinkIcon from 'assets/icons/link.svg';
 
 const StyledWrapper = styled.div`
   min-height: 380px;
@@ -15,8 +17,13 @@ const StyledWrapper = styled.div`
 `;
 
 const InnerWrapper = styled.div`
+  position: relative;
   padding: 17px 30px;
-  background-color: ${({ yellow, theme }) => (yellow ? theme.primary : 'white')};
+  background-color: ${({ activeColor, theme }) => (activeColor ? theme[activeColor] : 'white')};
+
+  :first-of-type {
+    z-index: 9999;
+  }
 
   ${({ flex }) =>
     flex &&
@@ -37,11 +44,39 @@ const StyledHeading = styled(Heading)`
   margin: 10px 0 0;
 `;
 
-const Card = () => (
+const StyledAvatar = styled.img`
+  width: 86px;
+  height: 86px;
+  border-radius: 50px;
+  border: 5px solid ${({ theme }) => theme.twitter};
+  position: absolute;
+  right: 25px;
+  top: 25px;
+`;
+
+const StyledLinkButton = styled.a`
+  display: block;
+  height: 47px;
+  width: 47px;
+  border-radius: 50px;
+  background: white url(${LinkIcon}) no-repeat;
+  background-size: 60%;
+  background-position: 50%;
+  position: absolute;
+  right: 25px;
+  top: 50px;
+  transform: translateY(-50%);
+`;
+
+const Card = ({ cardType }) => (
   <StyledWrapper>
-    <InnerWrapper yellow>
-      <StyledHeading>Hello Roman</StyledHeading>
+    <InnerWrapper activeColor={cardType}>
+      <StyledHeading>Hello world</StyledHeading>
       <DateInfo>3 days left</DateInfo>
+      {cardType === 'twitter' && <StyledAvatar src="https://avatars.io/twitter/hello_roman" />}
+      {cardType === 'article' && (
+        <StyledLinkButton href="https://www.youtube.com/channel/UCq8XmOMtrUCb8FcFHQEd8_g/featured" />
+      )}
     </InnerWrapper>
     <InnerWrapper flex>
       <Paragraph>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, corrupti!</Paragraph>
@@ -49,5 +84,13 @@ const Card = () => (
     </InnerWrapper>
   </StyledWrapper>
 );
+
+Card.propTypes = {
+  cardType: PropTypes.oneOf(['note', 'twitter', 'article']),
+};
+
+Card.defaultProps = {
+  cardType: 'note',
+};
 
 export default Card;
--- src/components/molecules/Card/Card.stories.js
+++ src/components/molecules/Card/Card.stories.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Card from './Card';
 
-storiesOf('Card', module).add('Normal', () => <Card />);
+storiesOf('Card', module).add('Note', () => <Card cardType="note" />);
+storiesOf('Card', module).add('Twitter', () => <Card cardType="twitter" />);
+storiesOf('Card', module).add('Article', () => <Card cardType="article" />);
